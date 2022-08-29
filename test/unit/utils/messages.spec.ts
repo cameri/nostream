4cbbb0f5b27f70d03663a2a9dde7237f8d3afda6
Ricardo Arturo Cabral Mejia <me@ricardocabral.io>
2022-08-29 03:46:37 +0000

    test: message schema

---
+++ test/unit/utils/messages.spec.ts
@@ -0,0 +1,27 @@
+import { expect } from 'chai'
+
+import { createEndOfStoredEventsNoticeMessage, createNotice, createOutgoingEventMessage } from '../../../src/utils/messages'
+import { Event } from '../../../src/@types/event'
+import { MessageType } from '../../../src/@types/messages'
+
+describe('createNotice', () => {
+  it('returns a notice message', () => {
+    expect(createNotice('some notice')).to.deep.equal([MessageType.NOTICE, 'some notice'])
+  })
+})
+
+describe('createOutgoingEventMessage', () => {
+  it('returns an event message', () => {
+    const event: Event = {
+      id: 'some id',
+    } as any
+    expect(createOutgoingEventMessage('subscriptionId', event)).to.deep.equal([MessageType.EVENT, 'subscriptionId', event])
+  })
+})
+
+describe('createEndOfStoredEventsNoticeMessage', () => {
+  it('returns a EOSE message', () => {
+    expect(createEndOfStoredEventsNoticeMessage('subscriptionId')).to.deep.equal([MessageType.EOSE, 'subscriptionId'])
+  })
+})
+
