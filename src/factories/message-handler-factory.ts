515c7dd4429ea889bc0bef1cb6b0d4db12dfc965
Ricardo Arturo Cabral Mejia <me@ricardocabral.io>
2022-10-03 01:48:54 +0000

    chore: inject settings into EventMessageHandler

--- src/factories/message-handler-factory.ts
+++ src/factories/message-handler-factory.ts
@@ -3,16 +3,16 @@ import { EventMessageHandler } from '../handlers/event-message-handler'
 import { eventStrategyFactory } from './event-strategy-factory'
 import { IEventRepository } from '../@types/repositories'
 import { IWebSocketAdapter } from '../@types/adapters'
+import { Settings } from '../utils/settings'
 import { SubscribeMessageHandler } from '../handlers/subscribe-message-handler'
 import { UnsubscribeMessageHandler } from '../handlers/unsubscribe-message-handler'
-
 
 export const messageHandlerFactory = (
   eventRepository: IEventRepository,
 ) => ([message, adapter]: [IncomingMessage, IWebSocketAdapter]) => {
   switch (message[0]) {
     case MessageType.EVENT:
-      return new EventMessageHandler(adapter, eventStrategyFactory(eventRepository))
+      return new EventMessageHandler(adapter, eventStrategyFactory(eventRepository), Settings)
     case MessageType.REQ:
       return new SubscribeMessageHandler(adapter, eventRepository)
     case MessageType.CLOSE:
