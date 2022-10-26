bdcab652b0676bde01b72cac6247da5e76582bf6
Ricardo Arturo Cabral Mejia <me@ricardocabral.io>
2022-10-26 18:56:03 -0400

    chore: refactor worker & app factory

--- src/app/worker.ts
+++ src/app/worker.ts
@@ -1,34 +1,13 @@
-import http from 'http'
-import process from 'process'
-import { WebSocketServer } from 'ws'
-
-import { createSettings } from '../factories/settings-factory'
-import { IEventRepository } from '../@types/repositories'
 import { IRunnable } from '../@types/base'
-import { webSocketAdapterFactory } from '../factories/websocket-adapter-factory'
-import { WebSocketServerAdapter } from '../adapters/web-socket-server-adapter'
+import { IWebSocketServerAdapter } from '../@types/adapters'
 
 export class AppWorker implements IRunnable {
-  private webServer: http.Server
-  private wss: WebSocketServer
-  private adapter: WebSocketServerAdapter
-
   public constructor(
-    private readonly eventRepository: IEventRepository
+    private readonly process: NodeJS.Process,
+    private readonly adapter: IWebSocketServerAdapter
   ) {
-    this.webServer = http.createServer()
-    this.wss = new WebSocketServer({ server: this.webServer, maxPayload: 1024 * 1024 })
-    this.adapter = new WebSocketServerAdapter(
-      this.webServer,
-      this.wss,
-      webSocketAdapterFactory(this.eventRepository),
-      createSettings,
-    )
-
     process
-      .on('message', (message: { eventName: string, event: unknown }) => {
-        this.adapter.emit(message.eventName, message.event)
-      })
+      .on('message', this.onMessage.bind(this))
       .on('SIGINT', this.onExit.bind(this))
       .on('SIGHUP', this.onExit.bind(this))
       .on('SIGTERM', this.onExit.bind(this))
@@ -44,6 +23,10 @@ export class AppWorker implements IRunnable {
     console.log(`worker ${process.pid} - listening on port ${port}`)
   }
 
+  private onMessage(message: { eventName: string, event: unknown }): void {
+    this.adapter.emit(message.eventName, message.event)
+  }
+
   private onError(error: Error) {
     console.error(`worker ${process.pid} - error`, error)
     throw error
@@ -51,13 +34,11 @@ export class AppWorker implements IRunnable {
 
   private onExit() {
     console.log(`worker ${process.pid} - exiting`)
-    this.wss.close(() => {
-      this.webServer.close(() => {
-        // dbClient.destroy(() => {
-        //   process.exit(0)
-        // })
-        process.exit(0)
-      })
+    this.adapter.close(() => {
+      // dbClient.destroy(() => {
+      //   process.exit(0)
+      // })
+      process.exit(0)
     })
   }
 }
--- src/factories/worker-factory.ts
+++ src/factories/worker-factory.ts
@@ -1,12 +1,29 @@
+import http from 'http'
+import process from 'process'
+import { WebSocketServer } from 'ws'
+
 import { AppWorker } from '../app/worker'
+import { createSettings } from '../factories/settings-factory'
 import { EventRepository } from '../repositories/event-repository'
 import { getDbClient } from '../database/client'
+import { webSocketAdapterFactory } from './websocket-adapter-factory'
+import { WebSocketServerAdapter } from '../adapters/web-socket-server-adapter'
 
 export const workerFactory = () => {
   const dbClient = getDbClient()
   const eventRepository = new EventRepository(dbClient)
 
-  return new AppWorker(
-    eventRepository
+  const server = http.createServer()
+  const webSocketServer = new WebSocketServer({
+    server,
+    maxPayload: 1024 * 1024,
+  })
+  const adapter = new WebSocketServerAdapter(
+    server,
+    webSocketServer,
+    webSocketAdapterFactory(eventRepository),
+    createSettings,
   )
+
+  return new AppWorker(process, adapter)
 }
