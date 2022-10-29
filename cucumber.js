321e65014e063e0f79b7476a8a155bddefc48efb
Ricardo Arturo Cabral Mejia <me@ricardocabral.io>
2022-10-28 20:51:09 -0400

    chore(ci): add intg tests coverage

--- cucumber.js
+++ cucumber.js
@@ -1,12 +1,19 @@
-const config = [
+const base = [
   'test/integration/features/**/*.feature',
   '--require-module ts-node/register',
   '--require test/integration/features/**/*.ts',
   '--require test/integration/features/*.ts',
+].join(' ')
+
+const config = [
+  base,
   '--format @cucumber/pretty-formatter',
+  '--format html:.test-reports/integration/report.html',
+  '--format json:.test-reports/integration/report.json',
   '--publish',
 ].join(' ')
 
 module.exports = {
   default: config,
+  cover: base,
 }
