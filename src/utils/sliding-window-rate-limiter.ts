b58d78a735253c5d3b9e33b0d49f825abe230f14
Ricardo Arturo Cabral Mejía <me@ricardocabral.io>
2022-11-14 23:00:54 -0500

    feat: add sliding window rate limiter

---
+++ src/utils/sliding-window-rate-limiter.ts
@@ -0,0 +1,35 @@
+import { IRateLimiter, IRateLimiterOptions } from '../@types/utils'
+import { createLogger } from '../factories/logger-factory'
+import { ICacheAdapter } from '../@types/adapters'
+
+const debug = createLogger('sliding-window-rate-limiter')
+
+export class SlidingWindowRateLimiter implements IRateLimiter {
+  public constructor(
+    private readonly cache: ICacheAdapter,
+  ) {}
+
+  public async hit(
+    key: string,
+    step: number,
+    options: IRateLimiterOptions,
+  ): Promise<boolean> {
+    const timestamp = Date.now()
+    const { period } = options
+
+    debug('add %d hits on %s bucket', step, key)
+
+    const [,, entries] = await Promise.all([
+      this.cache.removeRangeByScoreFromSortedSet(key, 0, timestamp - period),
+      this.cache.addToSortedSet(key, { [`${timestamp}:${step}`]: timestamp.toString() }),
+      this.cache.getRangeFromSortedSet(key, 0, -1),
+      this.cache.setKeyExpiry(key, period),
+    ])
+
+    const hits = entries.reduce((acc, timestampAndStep) => acc + Number(timestampAndStep.split(':')[1]), 0)
+
+    debug('hit count on %s bucket: %d', key, hits)
+
+    return hits > options.rate
+  }
+}
