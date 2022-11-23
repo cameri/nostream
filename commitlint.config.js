e646ff140e6098525aba554ec17e2f1d1b7f6643
Ricardo Arturo Cabral Mejía <me@ricardocabral.io>
2022-11-22 19:45:18 -0500

    chore(ci): use commitlint-github-action
    Signed-off-by: Ricardo Arturo Cabral Mejía <me@ricardocabral.io>

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,7 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional']
+  extends: ['@commitlint/config-conventional'],
+	parserPreset: 'conventional-changelog-conventionalcommits',
+	rules: {
+		'body-max-line-length': [2, 'always', 250],
+  },
 }
