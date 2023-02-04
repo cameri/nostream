20f62e0a58baed981b3ad5b29385a2acd1ae179e
Ricardo Arturo Cabral Mejía <me@ricardocabral.io>
2023-02-04 13:18:48 -0500

    feat: support nip-111

--- src/handlers/request-handlers/root-request-handler.ts
+++ src/handlers/request-handlers/root-request-handler.ts
@@ -2,6 +2,7 @@ import { NextFunction, Request, Response } from 'express'
 import { path } from 'ramda'
 
 import { createSettings } from '../../factories/settings-factory'
+import { FeeSchedule } from '../../@types/settings'
 import packageJson from '../../../package.json'
 
 export const rootRequestHandler = (request: Request, response: Response, next: NextFunction) => {
@@ -9,8 +10,12 @@ export const rootRequestHandler = (request: Request, response: Response, next: N
 
   if (request.header('accept') === 'application/nostr+json') {
     const {
-      info: { name, description, pubkey, contact },
+      info: { name, description, pubkey, contact, relay_url },
     } = settings
+
+    const paymentsUrl = new URL(relay_url)
+    paymentsUrl.protocol = paymentsUrl.protocol === 'wss:' ? 'https:' : 'http:'
+    paymentsUrl.pathname = '/invoices'
 
     const relayInformationDocument = {
       name,
@@ -20,6 +25,33 @@ export const rootRequestHandler = (request: Request, response: Response, next: N
       supported_nips: packageJson.supportedNips,
       software: packageJson.repository.url,
       version: packageJson.version,
+      limitation: {
+            max_message_length: settings.network.maxPayloadSize,
+            max_subscriptions: settings.limits.client.subscription.maxSubscriptions,
+            max_filters: settings.limits.client.subscription.maxFilters,
+            max_limit: 5000,
+            max_subid_length: 256,
+            min_prefix: 4,
+            max_event_tags: 2500,
+            max_content_length: 102400,
+            min_pow_difficulty: settings.limits.event.eventId.minLeadingZeroBits,
+            auth_required: false,
+            payment_required: settings.payments.enabled,
+      },
+      payments_url: paymentsUrl.toString(),
+      fees: Object
+        .getOwnPropertyNames(settings.payments.feeSchedules)
+        .reduce((prev, feeName) => {
+          const feeSchedules = settings.payments.feeSchedules[feeName] as FeeSchedule[]
+
+          return {
+            ...prev,
+            [feeName]: feeSchedules.reduce((fees, fee) => (fee.enabled)
+              ? [...fees, { amount: fee.amount, unit: 'msats' }]
+              : fees, []),
+          }
+
+        }, {} as Record<string, { amount: number, unit: string }>),
     }
 
     response
