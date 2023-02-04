b8c35364a7dccec520721b886cd80d3e0eb06548
Ricardo Arturo Cabral Mejía <me@ricardocabral.io>
2023-02-03 19:17:24 -0500

    fix: disable commitlint case rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -3,5 +3,8 @@ module.exports = {
 	parserPreset: 'conventional-changelog-conventionalcommits',
 	rules: {
 		'body-max-line-length': [2, 'always', 250],
+		'subject-case': [0, 'always'],
+		'header-case': [0, 'always'],
+		'body-case': [0, 'always'],
   },
 }
