ed1e5884b3bb9a54eec7ad1e874c52f845a7f9fc
Kanishka.. <143314565+kanishka0411@users.noreply.github.com>
2026-04-09 03:55:55 +0530

    fix: set event_deduplication in seed script (#386)

--- seeds/0000-events.js
+++ seeds/0000-events.js
@@ -1,6 +1,32 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
 const NAMESPACE = 'c646b451-db73-47fb-9a70-ea24ce8a225a'
+
+function isReplaceableEvent(kind) {
+  return kind === 0
+    || kind === 3
+    || kind === 41
+    || (kind >= 10000 && kind < 20000)
+}
+
+function isParameterizedReplaceableEvent(kind) {
+  return kind >= 30000 && kind < 40000
+}
+
+function getEventDeduplication(event) {
+  if (isReplaceableEvent(event.kind)) {
+    return JSON.stringify([event.pubkey, event.kind])
+  }
+
+  if (isParameterizedReplaceableEvent(event.kind)) {
+    const dTag = event.tags.find((tag) => tag.length >= 2 && tag[0] === 'd')
+    const [, ...deduplication] = dTag ?? [null, '']
+    return JSON.stringify(deduplication)
+  }
+
+  return null
+}
+
 exports.seed = async function (knex) {
   await knex('events').del()
 
@@ -16,6 +42,7 @@ exports.seed = async function (knex) {
       event_content: event.content,
       event_tags: JSON.stringify(event.tags),
       event_signature: Buffer.from(event.sig, 'hex'),
+      event_deduplication: getEventDeduplication(event),
     })
 
     return result
