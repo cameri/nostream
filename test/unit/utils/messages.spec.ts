00393df205faa1f366454409eec9d9fecffbd9d8
Priyanshubhartistm <bhartipriyanshustm@gmail.com>
2026-04-22 07:49:52 +0530

    test: fix changeset metadata and add messages util tests

--- test/unit/utils/messages.spec.ts
+++ test/unit/utils/messages.spec.ts
@@ -1,7 +1,14 @@
 import { expect } from 'chai'
 
-import { createEndOfStoredEventsNoticeMessage, createNoticeMessage, createOutgoingEventMessage } from '../../../src/utils/messages'
-import { Event } from '../../../src/@types/event'
+import {
+  createCommandResult,
+  createEndOfStoredEventsNoticeMessage,
+  createNoticeMessage,
+  createOutgoingEventMessage,
+  createRelayedEventMessage,
+  createSubscriptionMessage,
+} from '../../../src/utils/messages'
+import { Event, RelayedEvent } from '../../../src/@types/event'
 import { MessageType } from '../../../src/@types/messages'
 
 describe('createNotice', () => {
@@ -22,6 +29,47 @@ describe('createOutgoingEventMessage', () => {
 describe('createEndOfStoredEventsNoticeMessage', () => {
   it('returns a EOSE message', () => {
     expect(createEndOfStoredEventsNoticeMessage('subscriptionId')).to.deep.equal([MessageType.EOSE, 'subscriptionId'])
+  })
+})
+
+describe('createCommandResult', () => {
+  it('returns a command result message', () => {
+    expect(createCommandResult('event-id', true, 'accepted')).to.deep.equal([
+      MessageType.OK,
+      'event-id',
+      true,
+      'accepted',
+    ])
+  })
+})
+
+describe('createSubscriptionMessage', () => {
+  it('returns a subscription message with filters', () => {
+    const filters = [{ authors: ['author-1'], kinds: [1], '#p': ['recipient-1'] }]
+
+    expect(createSubscriptionMessage('subscriptionId', filters)).to.deep.equal([
+      MessageType.REQ,
+      'subscriptionId',
+      ...filters,
+    ])
+  })
+})
+
+describe('createRelayedEventMessage', () => {
+  const event: RelayedEvent = {
+    id: 'event-id',
+  } as any
+
+  it('returns an EVENT message without secret when secret is missing', () => {
+    expect(createRelayedEventMessage(event)).to.deep.equal([MessageType.EVENT, event])
+  })
+
+  it('returns an EVENT message with secret when provided', () => {
+    expect(createRelayedEventMessage(event, 'shared-secret')).to.deep.equal([
+      MessageType.EVENT,
+      event,
+      'shared-secret',
+    ])
   })
 })
 
